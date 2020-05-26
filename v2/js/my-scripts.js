41034178fbeb557239282acecb3717dfd2ecd937
Fred3rick <62725164+Fred3rick@users.noreply.github.com>
2020-05-26 23:20:30 +0200

    Sito aggiornato alla versione 2 con inserimento di Bootstrap 3
    Sito messo in sottocartella

---
+++ v2/js/my-scripts.js
@@ -0,0 +1,60 @@
+/* Change Icon
+$(document).ready(function() {
+  $(".header__hamburger").click(function() {
+    $(this).toggleClass("header__hamburger--active");
+  });
+}); */
+
+/* Change color on refresh */
+$(document).ready(function() {
+  var colors = ["#4285F4", "#EA4335", "#34A853", "#F9AB00"];
+  var rand = Math.floor(Math.random() * colors.length);
+  $('.thumbnail').css("background", colors[rand]);
+  $('.bar').css("background-color", colors[rand]);
+  $('.progress-bar').css("background-color", colors[rand]);
+});
+
+/* Greeting
+var myDate = new Date();
+var hrs = myDate.getHours();
+var greet;
+if (hrs < 12)
+  greet = 'Buongiorno';
+else if (hrs >= 12 && hrs <= 17)
+  greet = 'Buon pomeriggio';
+else if (hrs >= 17 && hrs <= 24)
+  greet = 'Buonasera';
+document.getElementById('myGreet').innerHTML = greet;*/
+
+/*Real-Time clock
+function clock() {
+  var time = new Date(),
+    hours = time.getHours(),
+    minutes = time.getMinutes(),
+    seconds = time.getSeconds();
+  document.querySelectorAll('.clock')[0].innerHTML = harold(hours) + ":" + harold(minutes) + ":" + harold(seconds);
+
+  function harold(standIn) {
+    if (standIn < 10) {
+      standIn = '0' + standIn
+    }
+    return standIn;
+  }
+}
+setInterval(clock, 1000);*/
+
+/*Date of today
+var today = new Date();
+var day = today.getDate();
+var month = today.getMonth() + 1;
+var year = today.getFullYear();
+
+if (day < 10) {
+  day = '0' + day
+}
+if (month < 10) {
+  month = '0' + month
+}
+
+var out = document.getElementById("today");
+out.innerHTML = day + "/" + month + "/" + year;*/
